39192bec9fc6e7b64cbde65c6fe8b0d92542d0fc
Lanaya Sunil <lanayasunil097@gmail.com>
2025-06-16 16:27:37 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,50 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify,request
+import mysql.connector
 
 app = Flask(__name__)
 
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",  # Default XAMPP MySQL password
+        database="HRMS"
+    )
+
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
+
+@app.route("/api/employees")
+def get_employees():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT employee.Emp_ID, employee.First_Name, employee.Last_Name, department.Name FROM `employee` Left JOIN department on department.ID = employee.Department_ID;")
+    employees = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(employees)
+
+@app.route("/api/methods")
+def get_methods():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT Method FROM method;") 
+    methods = cursor.fetchall()
+    conn.close()
+    return jsonify(methods)
+
+@app.route("/api/employee-names")
+def get_employee_names():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT Emp_ID, First_Name, Last_Name FROM employee;")
+    employees = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(employees)
+
+
 
 @app.route("/submit-report", methods=["POST"])
 def submit_report():
@@ -27,6 +67,8 @@ def submit_report():
         print("New Manager:", newmanager)
 
     return jsonify({"message": "Report submitted successfully!"}), 200
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8081)
